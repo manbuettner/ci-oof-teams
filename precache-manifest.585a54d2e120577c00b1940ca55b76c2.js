c4315b02cb6ef52f0f96a914bd9db6d2afc290c5
manbuettner <m.buettner@ci-solution.com>
2021-01-21 08:14:25 +0100

    Updates

---
+++ precache-manifest.585a54d2e120577c00b1940ca55b76c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6aae7e9ffdc28759c050ca497e6628",
+    "url": "https://manbuettner.github.io/index.html"
+  },
+  {
+    "revision": "f16294a72e4f810a329a",
+    "url": "https://manbuettner.github.io/static/css/2.b06b5bd6.chunk.css"
+  },
+  {
+    "revision": "1dfd453b08112b6bea3a",
+    "url": "https://manbuettner.github.io/static/css/main.12de4a83.chunk.css"
+  },
+  {
+    "revision": "f16294a72e4f810a329a",
+    "url": "https://manbuettner.github.io/static/js/2.27836458.chunk.js"
+  },
+  {
+    "revision": "7529dbc6825d5c46891c4740009c3f5b",
+    "url": "https://manbuettner.github.io/static/js/2.27836458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dfd453b08112b6bea3a",
+    "url": "https://manbuettner.github.io/static/js/main.da1cba1c.chunk.js"
+  },
+  {
+    "revision": "baff82f6c0240996fe5a",
+    "url": "https://manbuettner.github.io/static/js/runtime-main.c24d0301.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://manbuettner.github.io/precache-manifest.7d431e20dce6c11827933dfc3bb7a3fa.js"
+  "https://manbuettner.github.io/precache-manifest.585a54d2e120577c00b1940ca55b76c2.js"
 );
 
 self.addEventListener('message', (event) => {
